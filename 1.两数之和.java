72f192729e7db5d9576ca2e68109e413a5fd7647
Enidwonder <enid1999@163.com>
2020-10-22 21:21:59 +0800

    first commit test

---
+++ 1.两数之和.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        for(int i = 0;i < nums;i ++){
+
+        }
+    }
+}
+// @lc code=end
+
